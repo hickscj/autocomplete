b57118513938598937a23d5c07f718a8c03c7098
Chad Hicks <chadjhicks@gmail.com>
2021-02-08 03:34:32 -0500

    Add more flesh to api

--- app/server.js
+++ app/server.js
@@ -1,11 +1,42 @@
+'use strict';
+
 const express = require('express');
 const app = express();
+const querystring = require('querystring');
 
-app.get("/", (req, res) => {
-    res.send('hi there express!');
-});
+const { MongoClient } = require("mongodb");
+const url = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017";
+const dbName = "autocomplete";
+const collectionName = "words";
 
-app.listen(3000, () => {
-    console.log('server started');
-});
+async function start() {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const words = db.collection(collectionName);
 
+    app.get("/", (req, res) => {
+        res.json({'message': 'autocomplete api home'});
+    });
+
+    app.get("/set-words", async (req, res) => {
+        const c = await words.insertOne({'word': 'a', 'weight': 0});
+        res.send(c);
+    });
+
+    app.get("/see", async (req, res) => {
+        const myCursor = await words.find({});
+        let stuff = [];
+        let safety = 0;
+        while (myCursor.hasNext() && safety < 3) {
+            safety++;
+            stuff.push(await myCursor.next());
+        }
+        res.json(stuff);
+    })
+
+    app.listen(3000, () => {
+        console.log('server started');
+    });
+}
+
+start();
