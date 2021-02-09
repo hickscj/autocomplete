01f19e982f24605d6d9cffcb6d76f62035f3dd04
Chad Hicks <chadjhicks@gmail.com>
2021-02-09 01:37:36 -0500

    API updates to search and return data object

--- app/server.js
+++ app/server.js
@@ -2,40 +2,60 @@
 
 const express = require('express');
 const app = express();
-const querystring = require('querystring');
+// const querystring = require('querystring');
 
 const { MongoClient } = require("mongodb");
 const url = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017";
 const dbName = "autocomplete";
 const collectionName = "words";
 
+const baseURL = '/api/v1';
+const port = 3000;
+
 async function start() {
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
     const words = db.collection(collectionName);
 
-    app.get("/", (req, res) => {
-        res.json({'message': 'autocomplete api home'});
+    app.get(baseURL + "/test", (req, res) => {
+        res.json({'message': 'API test success!'});
     });
 
-    app.get("/set-words", async (req, res) => {
-        const c = await words.insertOne({'word': 'a', 'weight': 0});
-        res.send(c);
-    });
-
-    app.get("/see", async (req, res) => {
-        const myCursor = await words.find({});
-        let stuff = [];
-        let safety = 0;
-        while (myCursor.hasNext() && safety < 3) {
-            safety++;
-            stuff.push(await myCursor.next());
+    app.get(baseURL + "/add-weight", (req, res) => {
+        if(req.query.amount && req.query.word) {
+            console.log(`Adding ${req.query.amount} to word ${req.query.word}`);
+            res.send('added weight');
         }
-        res.json(stuff);
+        res.send('FAILED to add weight ');
+    });
+
+    app.get(baseURL + "/search", (req, res) => {
+        const origin = req.get('origin');
+
+        console.log(origin);
+
+        const searchTerm = req.query.q;
+        const re = `^${searchTerm}`;
+        const searchRegex = new RegExp(re, 'gi');
+        const searchLimit = req.query.limit || 8;
+        console.log(searchTerm);
+        
+        // when developing the frontend with parcel build, allow that origin
+        if(origin === 'http://localhost:1234') res.header('Access-Control-Allow-Origin', origin);
+
+        words.find({
+            "word": { $regex: searchRegex }
+        }, {"word": true})
+            .limit(searchLimit)
+            .toArray( (err, docs) => {
+                res.header('Access-Control-Allow-Origin', origin);
+                if(err) return res.send(err.message);
+                res.send(docs);
+            });
     })
 
-    app.listen(3000, () => {
-        console.log('server started');
+    app.listen(port, () => {
+        console.log('autocomplete api server started...');
     });
 }
 
