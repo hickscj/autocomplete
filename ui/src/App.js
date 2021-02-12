9e303ac8e29699cdeb2eb2298aafc5410c691d84
Chad Hicks <chadjhicks@gmail.com>
2021-02-12 11:07:09 -0500

    A little better error handling

--- ui/src/App.js
+++ ui/src/App.js
@@ -10,9 +10,20 @@ const App = () => {
         if(evt.keyCode >= 65 && evt.keyCode <= 90) {
             const searchText = document.getElementById('searchText').value;
             if(searchText.length > 0) {
-                const res = await fetch(`http://localhost:3000/api/v1/search?q=${searchText}`);
-                const parsed = await res.json();
-                setWords(parsed);
+                try {
+                    const res = await fetch(`http://localhost:3000/api/v1/search?q=${searchText}`);
+                    const parsed = await res.json();
+                    setWords(parsed);
+                } catch(err) {
+                    const errorParts = err.message.split(' ');
+                    console.error(`Error fetching... ${err}`);
+                    if(errorParts.includes('NetworkError')) {
+                        setWords([{
+                            _id: 0,
+                            word: 'There was an error connecting.\n Did you remember to start the API server?'
+                        }]);
+                    }
+                }
             }
         }
         if(evt.key === "Escape" || evt.key === "Enter") setWords([]);
